9da636bbb4eb5824d706269275de970ad0b28c2d
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-03-10 23:42:23 +0300

    Manage a list of users via state in the App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { Fragment, useState } from 'react';
+
+import AddUser from './Components/Users/AddUser';
+import UserList from './Components/Users/UserList';
+
 const App = () => {
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUserList) => (
+      [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() }
+      ]
+    ));
+  };
+
   return (
-    <div>Hello!</div>
+    <Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
+    </Fragment>
   );
 };
 
